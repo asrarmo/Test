8fcf097425e351fb0f9957442c028b2b95ed0318
asrarmo <72375376+asrarmo@users.noreply.github.com>
2020-10-05 15:58:41 +0530

    some random text added

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
 public static void main(String args[]){
 system.out.println("Hello World");
   system.out.println("Good Evening");
+  system.out.println("Some random text");
 }
 }
